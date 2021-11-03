93ae0e8903140a57ca4863a15cc1ad0b97f0dc84
Nicolas Vanhecke <nicolas.vanhecke@me.com>
2021-11-03 20:13:03 +0100

    Tailwind tables

--- resources/views/admin/flights/index.blade.php
+++ resources/views/admin/flights/index.blade.php
@@ -9,34 +9,34 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <h1>All flights</h1>
 
-                    <table class="table">
+                    <table class="table-auto">
                         <thead>
                             <tr>
-                                <th>FlightNr</th>
-                                <th>CommercialNr</th>
-                                <th>Departure</th>
-                                <th>Arrival</th>
-                                <th>STD</th>
-                                <th>STA</th>
-                                <th>Last update</th>
+                                <th class="text-left border-b-4 px-4 py-2">FlightNr</th>
+                                <th class="text-left border-b-4 px-4 py-2">CommercialNr</th>
+                                <th class="text-left border-b-4 px-4 py-2">Departure</th>
+                                <th class="text-left border-b-4 px-4 py-2">Arrival</th>
+                                <th class="text-left border-b-4 px-4 py-2">STD</th>
+                                <th class="text-left border-b-4 px-4 py-2">STA</th>
+                                <th class="text-left border-b-4 px-4 py-2">Last update</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach( $flights as $flight )
                             <tr>
-                                <td><a href="{{ url('/admin/flights/' . $flight->id) }}"><b>{{ $flight->flight_number }}</b></a></td>
-                                <td>{{ $flight->commercial_number }}</td>
-                                <td>{{ $flight->departure }}</td>
-                                <td>{{ $flight->arrival }}</td>
-                                <td>{{ $flight->std }}</td>
-                                <td>{{ $flight->sta }}</td>
-                                <td>{{ $flight->updated_at }}</td>
+                                <td class="border-b-2 px-4 py-2"><a href="{{ url('/admin/flights/' . $flight->id) }}"><b>{{ $flight->flight_number }}</b></a></td>
+                                <td class="border-b-2 px-4 py-2">{{ $flight->commercial_number }}</td>
+                                <td class="border-b-2 px-4 py-2">{{ $flight->departure }}</td>
+                                <td class="border-b-2 px-4 py-2">{{ $flight->arrival }}</td>
+                                <td class="border-b-2 px-4 py-2">{{ $flight->std }}</td>
+                                <td class="border-b-2 px-4 py-2">{{ $flight->sta }}</td>
+                                <td class="border-b-2 px-4 py-2">{{ $flight->updated_at }}</td>
                             </tr>
                             @endforeach
                         </tbody>
                     </table>
+                    
                 </div>
             </div>
         </div>
--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -9,30 +9,30 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <h1>All messages</h1>
 
-                    <table class="table">
+                    <table class="table-auto">
                         <thead>
                             <tr>
-                                <th>Short</th>
-                                <th>Start date</th>
-                                <th>End date</th>
-                                <th>Airport</th>
-                                <th>Created at</th>
+                                <th class="text-left border-b-4 px-4 py-2">Short</th>
+                                <th class="text-left border-b-4 px-4 py-2">Start date</th>
+                                <th class="text-left border-b-4 px-4 py-2">End date</th>
+                                <th class="text-left border-b-4 px-4 py-2">Airport</th>
+                                <th class="text-left border-b-4 px-4 py-2">Created at</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach( $messages as $message )
                             <tr>
-                                <td><a href="{{ url('/admin/messages/' . $message->id) }}"><b>{{ $message->short }}</b></a></td>
-                                <td>{{ $message->start_date }}</td>
-                                <td>{{ $message->end_date }}</td>
-                                <td>{{ $message->airport }}</td>
-                                <td>{{ $message->created_at }}</td>
+                                <td class="border-b-2 px-4 py-2"><a href="{{ url('/admin/messages/' . $message->id) }}"><b>{{ $message->short }}</b></a></td>
+                                <td class="border-b-2 px-4 py-2">{{ $message->start_date }}</td>
+                                <td class="border-b-2 px-4 py-2">{{ $message->end_date }}</td>
+                                <td class="border-b-2 px-4 py-2">{{ $message->airport }}</td>
+                                <td class="border-b-2 px-4 py-2">{{ $message->created_at }}</td>
                             </tr>
                             @endforeach
                         </tbody>
                     </table>
+                    
                 </div>
             </div>
         </div>
